f81a2d3fcc02e88653363d5eae892061c32058ec
isabelle <mllexartemis@gmail.com>
2021-10-11 17:25:55 +0200

    struct

--- include/utils.h
+++ include/utils.h
@@ -1,4 +1,18 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+typedef struct bloc *Bloc_t;
+
+struct
+{
+    int level;
+    char predecessor[32];
+    time_t timestamp; 
+    char operations_hash[32];
+    char context_hash[32];
+    char signature[128];
+} bloc;
+
+Bloc_t new_bloc(int l, char * pred, time_t t, char* ope, char* c, char*s);
+
 #endif
--- src/utils.c
+++ src/utils.c
@@ -9,3 +9,20 @@ enum message {
     BLOCK_STATE,
     INJECT_OPERATION
 };
+
+typedef struct Bloc_t
+{
+    
+    int level;
+    char predecessor[32];
+    time_t timestamp; 
+    char operations_hash[32];
+    char context_hash[32];
+    char signature[128];
+};
+
+Bloc_t *new_bloc(int l, char * pred, time_t t, char* ope, char* c, char*s) {
+    level
+
+}
+
