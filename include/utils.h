893af9bc13f37a782075664b4df2a57134c7722e
EmilieSiau <emilie.siau@gmail.com>
2021-10-11 17:29:35 +0200

    Corrections Utils

--- include/utils.h
+++ include/utils.h
@@ -1,18 +1,29 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+enum message {
+    GET_CURRENT_HEAD,
+    CURRENT_HEAD,
+    GET_BLOCK,
+    BLOCK,
+    GET_BLOCK_OPERATIONS,
+    BLOCK_OPERATIONS,
+    GET_BLOCK_STATE,
+    BLOCK_STATE,
+    INJECT_OPERATION
+};
+
 typedef struct bloc *Bloc_t;
 
-struct
-{
+struct {
     int level;
     char predecessor[32];
-    time_t timestamp; 
+    long timestamp; 
     char operations_hash[32];
     char context_hash[32];
     char signature[128];
 } bloc;
 
-Bloc_t new_bloc(int l, char * pred, time_t t, char* ope, char* c, char*s);
+Bloc_t new_bloc(int l, char * pred, long t, char* ope, char* c, char*s);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,27 +1,6 @@
-enum message {
-    GET_CURRENT_HEAD,
-    CURRENT_HEAD,
-    GET_BLOCK,
-    BLOCK,
-    GET_BLOCK_OPERATIONS,
-    BLOCK_OPERATIONS,
-    GET_BLOCK_STATE,
-    BLOCK_STATE,
-    INJECT_OPERATION
-};
+#include "utils.h"
 
-typedef struct Bloc_t
-{
-    
-    int level;
-    char predecessor[32];
-    time_t timestamp; 
-    char operations_hash[32];
-    char context_hash[32];
-    char signature[128];
-};
-
-Bloc_t *new_bloc(int l, char * pred, time_t t, char* ope, char* c, char*s) {
+Bloc_t new_bloc(int l, char * pred, long t, char* ope, char* c, char*s) {
     level
 
 }
