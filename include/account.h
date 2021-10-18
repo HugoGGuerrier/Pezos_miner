63feda04469560dc75fbf22b7f0d9c213fe93159
EmilieSiau <emilie.siau@gmail.com>
2021-10-18 16:38:35 +0200

    Corrections account.c, block.c, state.c

--- include/account.h
+++ include/account.h
@@ -3,7 +3,7 @@
 
 #include "utils.h"
 
-#define ACCOUNT_CODE_SIZE (32 + 5*4) 
+#define ACCOUNT_CODE_SIZE (KEY_SIZE + 5*4) 
 
 // Take an account and encode it as a byte sequence
 char *encode_account(Account_t account);
