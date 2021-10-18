8d0b3a212da62e7038fcc8fe3627e842eff87433
Valentin Zero <valentin.zero@protonmail.com>
2021-10-18 14:13:58 +0200

    Struct correction

--- src/state.c
+++ src/state.c
@@ -1,1 +1,11 @@
 #include "state.h"
+
+char *encode_state(State_t state) {
+    // TODO
+    return NULL;
+}
+
+State_t decode_state(char *data) {
+    // TODO
+    return NULL;
+}
--- src/tmp.c
+++ src/tmp.c
@@ -1,15 +1,16 @@
 #include "utils.h"
 
+// TODO: refactor de into multiple verification function ?
 int verify(Block_t b) {
-    Block_t pred = get_predecessor(b.level);
+    Block_t pred = get_predecessor(b.level); /* TODO: fonction */
 
     // Vérifier predecessor
-    char *pred_datas = get_block_datas(pred);
-    unsigned char *pred_hash_res = malloc(32); // TODO: free
+    char *pred_datas = encode_block(pred);
+    unsigned char *pred_hash_res = malloc(32);
     blake2b((void *) pred_hash_res, 32, (void *) pred_datas, 172, NULL, 0);
     if (compare_data(b.predecessor, pred_hash_res, 32, 32)) {
-
         free(pred_hash_res);
+
         // Vérifier timestamp
         long time = b.timestamp - pred.timestamp;
         if (time >= 600) {
@@ -19,10 +20,10 @@ int verify(Block_t b) {
             if /*TODO: si op hash ok*/ {
                 
                 // Vérifier state hash
-                State_t state = get_state();
-                char *state_datas = get_state_datas(state);
+                State_t state = get_state(); /* TODO: fonction */
+                char *state_datas = encode_state(state);
                 unsigned char *state_hash_res = malloc(32);
-                blake2b((void *) state_hash_res, 32, (void *) state_datas, /* 44 + 52 * account nb */, NULL, 0);
+                blake2b((void *) state_hash_res, 32, (void *) state_datas, (44 + state.nb_account_bytes), NULL, 0);
                 if (compare_data(b.context_hash, state_hash_res, 32, 32)) {
                     
                     free(state_hash_res);
