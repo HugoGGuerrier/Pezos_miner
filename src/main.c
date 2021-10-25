263742d14c1f6b1d45c24d9f19e01e1a4707f77f
EmilieSiau <emilie.siau@gmail.com>
2021-10-25 17:52:46 +0200

    Test suite - account

--- src/main.c
+++ src/main.c
@@ -7,14 +7,54 @@
 #include "client.h"
 #include "utils.h"
 #include "block.h"
+#include "account.h"
+#include "message.h"
+#include "operation.h"
+#include "state.h"
 
 void test_read_hex_string(){
     print_hex(read_hex_string("0f"), 1, "\n");
+    printf("===> OK\n");
 }
 
 
 void test_account() {
+    // Random account example
+    char *user_public_key = read_hex_string("aca76354de343ef09385e263fb59561855d3cbf167961c6955624d91aa7eecf5");
+    unsigned int level_pez = 1;
+    unsigned int timestamp_pez = 2;
+    unsigned int operations_hash_pez = 3;
+    unsigned int context_hash_pez = 4;
+    unsigned int signature_pez = 5;
+   
+    // Build the account
+    Account_t account = new_account(user_public_key, level_pez, timestamp_pez, operations_hash_pez, context_hash_pez, signature_pez);
 
+    printf("Account before encoding:\n");
+    //print_account
+
+    // Encode the account
+    char *encoded = encode_account(account);
+
+    // Compare to the expected encoding
+    char *encoded_expect = read_hex_string("aca76354de343ef09385e263fb59561855d3cbf167961c6955624d91aa7eecf50000000100000002000000030000000400000005"); 
+    assert(compare_data(encoded, ACCOUNT_CODE_SIZE, encoded_expect, ACCOUNT_CODE_SIZE));
+
+    // Decode the account
+    Account_t decoded = decode_account(encoded);
+
+    printf("Account after encoding & decoding:\n");
+    //print_account
+
+    // Compare to the expected decoded account (initial account)
+    assert(compare_data(user_public_key, KEY_SIZE, decoded->user_public_key, KEY_SIZE));
+    assert(compare_data(&level_pez, sizeof(int), &decoded->level_pez, sizeof(int)));
+    assert(compare_data(&timestamp_pez, sizeof(int), &decoded->timestamp_pez, sizeof(int)));
+    assert(compare_data(&operations_hash_pez, sizeof(int), &decoded->operations_hash_pez, sizeof(int)));
+    assert(compare_data(&context_hash_pez, sizeof(int), &decoded->context_hash_pez, sizeof(int)));
+    assert(compare_data(&signature_pez, sizeof(int), &decoded->signature_pez, sizeof(int)));
+    
+    printf("===> OK\n");
 }
 
 
@@ -30,68 +70,77 @@ void test_block() {
     // Build the block
     Block_t block = new_block(level, predecessor, timestamp, operations_hash, context_hash, signature);
 
+    printf("Block before encoding:\n");
     print_block(block);
 
     // Encode the block
     char *encoded = encode_block(block);
-    print_hex(encoded, BLOCK_CODE_SIZE, "\n");
     
     // Compare to the expected encoding
     char *encoded_expect = read_hex_string("0000002c1c80203a30e5de4d980cc555131d1b4a4750edc82c0c443179d88de1ae4f6cdf00000000616304e5000000000000000000000000000000000000000000000000000000000000000022a00d1c8c0fbaefedd71ddb83d455033efd259a8f0adf189b9f850a0d1945f2cc3faffc696c86db13d50752fdb7edd0ee1ce19ab350f60899939fc139d58996419c13b812b7f005fafaf23924d2f1df555036bc61e7b67cb679375e5756b306"); 
     assert(compare_data(encoded, BLOCK_CODE_SIZE, encoded_expect, BLOCK_CODE_SIZE));
+    free(encoded_expect);
 
     // Decode the block
     Block_t decoded = decode_block(encoded);
+    free(encoded);
 
+    printf("Block after encoding & decoding:\n");
     print_block(decoded);
 
     // Compare to the expected decoded block (initial block)
-    assert(compare_data(level, sizeof(int), decoded->level, sizeof(int)));
+    assert(compare_data(&level, sizeof(int), &decoded->level, sizeof(int)));
     assert(compare_data(predecessor, HASH_SIZE, decoded->predecessor, HASH_SIZE));
-    assert(compare_data(timestamp, sizeof(long), decoded->timestamp, sizeof(long)));
+    assert(compare_data(&timestamp, sizeof(long), &decoded->timestamp, sizeof(long)));
     assert(compare_data(operations_hash, HASH_SIZE, decoded->operations_hash, HASH_SIZE));
     assert(compare_data(context_hash, HASH_SIZE, decoded->context_hash, HASH_SIZE));
     assert(compare_data(signature, SIG_SIZE, decoded->signature, SIG_SIZE));
+
+    delete_block(block);
+    delete_block(decoded);
+
+    printf("===> OK\n");
 }
 
 void test_message() {
     
+    printf("===> OK\n");
 }
 
 void test_operation() {
 
+    printf("===> OK\n");
 }
 
 void test_state() {
     
+    printf("===> OK\n");
 }
 
 void tests() {
     printf("===============================================\n");
     printf("==================== TESTS ====================\n");
     printf("===============================================\n");
-    
-    printf("========== Test Read Hex String ==========\n");
+
+    printf("\n========== Test Read Hex String ==========\n");
     test_read_hex_string();
 
-    printf("========== Test Account ==========\n");
+    printf("\n========== Test Account ==========\n");
     test_account();
     
-    printf("OK\n========== Test Block ==========\n");
+    printf("\n========== Test Block ==========\n");
     test_block();
     
-    printf("OK\n========== Test Message ==========\n");
+    printf("\n========== Test Message ==========\n");
     test_message();
     
-    printf("OK\n========== Test Operation ==========\n");
+    printf("\n========== Test Operation ==========\n");
     test_operation();
     
-    printf("OK\n========== Test State ==========\n");
+    printf("\n========== Test State ==========\n");
     test_state();
-    
-    printf("OK\n");
-    
-    printf("=====================================\n");
+
+    printf("\n===============================================\n");
 }
 
 int main() {
