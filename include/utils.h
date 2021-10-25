62bbe755bf2704b0c14d241f42330a3f47be81f7
isabelleshao <47239377+isabelleshao@users.noreply.github.com>
2021-10-25 17:43:47 +0200

    print account

--- include/utils.h
+++ include/utils.h
@@ -6,14 +6,20 @@
 #define HASH_SIZE 32
 #define KEY_SIZE 32
 #define SIG_SIZE 64
-#define PUBLIC_KEY {0xac,0xa7,0x63,0x54,0xde,0x34,0x3e,0xf0,0x93,0x85,0xe2,0x63,0xfb,0x59,0x56,0x18,0x55,0xd3,0xcb,0xf1,0x67,0x96,0x1c,0x69,0x55,0x62,0x4d,0x91,0xaa,0x7e,0xec,0xf5}
-#define PRIVATE_KEY {0xd9,0x66,0x1d,0x1a,0x70,0x95,0x18,0xef,0x62,0x13,0x11,0x75,0x08,0xac,0xba,0xe0,0xc9,0xb0,0x4f,0x33,0x8d,0x01,0x7a,0x28,0x63,0x3b,0xc4,0xa1,0xc1,0x5e,0x13,0x69}
+#define PUBLIC_KEY                                                                                                                                                                                     \
+    {                                                                                                                                                                                                  \
+        0xac, 0xa7, 0x63, 0x54, 0xde, 0x34, 0x3e, 0xf0, 0x93, 0x85, 0xe2, 0x63, 0xfb, 0x59, 0x56, 0x18, 0x55, 0xd3, 0xcb, 0xf1, 0x67, 0x96, 0x1c, 0x69, 0x55, 0x62, 0x4d, 0x91, 0xaa, 0x7e, 0xec, 0xf5 \
+    }
+#define PRIVATE_KEY                                                                                                                                                                                    \
+    {                                                                                                                                                                                                  \
+        0xd9, 0x66, 0x1d, 0x1a, 0x70, 0x95, 0x18, 0xef, 0x62, 0x13, 0x11, 0x75, 0x08, 0xac, 0xba, 0xe0, 0xc9, 0xb0, 0x4f, 0x33, 0x8d, 0x01, 0x7a, 0x28, 0x63, 0x3b, 0xc4, 0xa1, 0xc1, 0x5e, 0x13, 0x69 \
+    }
 
 // ----- Types and enum defining -----
 
-
 // --- Enumeration for all message tag
-typedef enum {
+typedef enum
+{
     GET_CURRENT_HEAD,
     CURRENT_HEAD,
     GET_BLOCK,
@@ -25,9 +31,9 @@ typedef enum {
     INJECT_OPERATION,
 } Message_Type_t;
 
-
 // --- Enumeration for all server operation type
-typedef enum {
+typedef enum
+{
     BAD_PREDECESSOR,
     BAD_TIMESTAMP,
     BAD_OPERATIONS_HASH,
@@ -35,34 +41,34 @@ typedef enum {
     BAD_SIGNATURE,
 } Operation_Type_t;
 
-
 // --- Type that represents a block
 typedef struct block *Block_t;
 
-struct block {
+struct block
+{
     int level;
     char *predecessor;
-    long timestamp; 
+    long timestamp;
     char *operations_hash;
     char *context_hash;
     char *signature;
 };
 
-
 // --- Type that represents a message
 typedef struct message *Message_t;
 
-struct message {
+struct message
+{
     Message_Type_t tag;
     unsigned short data_size;
     char *data;
 };
 
-
 // --- Type that represent an operation
 typedef struct operation *Operation_t;
 
-struct operation {
+struct operation
+{
     Operation_Type_t op_type;
     unsigned short data_size;
     char *data;
@@ -70,20 +76,20 @@ struct operation {
     char *signature;
 };
 
-
 // --- Type thar represent an operations list
 typedef struct operations *Operations_t;
 
-struct operations {
+struct operations
+{
     Operation_t head;
     Operations_t tail;
 };
 
-
 // --- Type that represents an account
 typedef struct account *Account_t;
 
-struct account {
+struct account
+{
     char *user_public_key;
     unsigned int level_pez;
     unsigned int timestamp_pez;
@@ -92,29 +98,27 @@ struct account {
     unsigned int signature_pez;
 };
 
-
 // --- Type that represent an accout list
 typedef struct accounts *Accounts_t;
 
-struct accounts {
+struct accounts
+{
     Account_t head;
     Accounts_t tail;
 };
 
-
 // --- Type that represents a state
 typedef struct state *State_t;
 
-struct state {
+struct state
+{
     char *dictator_public_key;
     unsigned long predecessor_timestamp;
     unsigned int nb_account_bytes;
     Accounts_t accounts;
 };
 
-
 // ----- Utils function definitions -----
-
 
 // --- Data creation and deletion functions
 
@@ -137,7 +141,7 @@ Operations_t new_operations(Operation_t head, Operations_t tail);
 void delete_operations(Operations_t operations);
 
 // Create a new block from the given data
-Block_t new_block(int level, char *pred, long time, char *ope_h, char *ctx_h, char* sig);
+Block_t new_block(int level, char *pred, long time, char *ope_h, char *ctx_h, char *sig);
 
 // Delete a block
 void delete_block(Block_t block);
@@ -160,7 +164,6 @@ Accounts_t new_accounts(Account_t head, Accounts_t tail);
 // Delete recursivly an account list
 void delete_accounts(Accounts_t accounts);
 
-
 // --- Printing functions
 
 // Print an operation
@@ -169,12 +172,14 @@ void print_op(Operation_t op);
 // Print a block
 void print_block(Block_t block);
 
+// Print an account
+void print_account(Account_t account);
+
 // Print a message
 void print_message(Message_t message);
 
 // Print a data in hexadecimal form
 void print_hex(char *thing, size_t size, const char *end);
-
 
 // --- Miscallenous functions
 
