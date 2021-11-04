a50c01aa02bd3e8ad5dfce0a16e0a2a846426a92
Hugo Guerrier <hugogguerrier@gmail.com>
2021-11-04 13:26:48 +0100

    Change return type for the rust operability

--- include/ed25519.h
+++ include/ed25519.h
@@ -1,9 +1,11 @@
 #ifndef ED25519_H
 #define ED25519_H
 
+#include <stdint.h>
+
 
 // Signing and verify functions
 void ed25519_sign(char *out, const char *in, unsigned long in_size, const char *private_key);
-int ed25519_verify(const char *signature, const char *message, unsigned long msg_size, const char *public_key);
+int32_t ed25519_verify(const char *signature, const char *message, unsigned long msg_size, const char *public_key);
 
 #endif
