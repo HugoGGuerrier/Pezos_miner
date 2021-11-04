c9d7b12f8deaaa48ff1d359811b4c4c3541a595a
Hugo Guerrier <hugogguerrier@gmail.com>
2021-11-04 14:04:27 +0100

    Add the public key to the sign function

--- include/ed25519.h
+++ include/ed25519.h
@@ -5,7 +5,7 @@
 
 
 // Signing and verify functions
-int32_t ed25519_sign(char *out, const char *in, uint64_t in_size, const char *private_key);
+int32_t ed25519_sign(char *out, const char *in, uint64_t in_size, const char *private_key, const char *public_key);
 int32_t ed25519_verify(const char *signature, const char *message, uint64_t msg_size, const char *public_key);
 
 #endif
